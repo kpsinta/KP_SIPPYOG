b1f48cbc414112960ebd6e2320e1859df8f9ddb7
sintaxd <jasintaxd@yahoo.com>
2019-12-26 14:41:31 +0700

    ui login

--- app/src/main/java/jls/com/sippyog/MainActivity.java
+++ app/src/main/java/jls/com/sippyog/MainActivity.java
@@ -1,10 +1,15 @@
 package jls.com.sippyog;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.design.widget.TextInputEditText;
+import android.support.design.widget.TextInputLayout;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
-
+    private Button btnLogin;
+    private TextInputLayout textInputUsername, textInputPassword;
+    private TextInputEditText input_username, input_password;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
