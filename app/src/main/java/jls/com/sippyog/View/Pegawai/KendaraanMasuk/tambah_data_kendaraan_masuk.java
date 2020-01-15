b331802b1bde26710e6b332a54d2463bb57648b4
sintaxd <jasintaxd@yahoo.com>
2020-01-15 15:55:43 +0700

    cicil fungsi kendaraan masuk, masih belum bisa generate POD

--- app/src/main/java/jls/com/sippyog/View/Pegawai/KendaraanMasuk/tambah_data_kendaraan_masuk.java
+++ app/src/main/java/jls/com/sippyog/View/Pegawai/KendaraanMasuk/tambah_data_kendaraan_masuk.java
@@ -6,6 +6,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.Spinner;
@@ -15,14 +16,23 @@ import android.widget.Toast;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 
+import java.text.DateFormat;
+import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 
 import jls.com.sippyog.API.ApiClient_Kendaraan;
+import jls.com.sippyog.API.ApiClient_KendaraanMasuk;
+import jls.com.sippyog.API.ApiClient_Shift;
 import jls.com.sippyog.ListData.LD_Kendaraan;
+import jls.com.sippyog.ListData.LD_Shift;
 import jls.com.sippyog.Model.Model_Kendaraan;
+import jls.com.sippyog.Model.Model_KendaraanMasuk;
+import jls.com.sippyog.Model.Model_Shift;
 import jls.com.sippyog.R;
+import jls.com.sippyog.SessionManager.SessionManager;
 import retrofit2.Call;
 import retrofit2.Callback;
 import retrofit2.Response;
@@ -31,25 +41,61 @@ import retrofit2.converter.gson.GsonConverterFactory;
 
 public class tambah_data_kendaraan_masuk extends AppCompatActivity {
 
-    Spinner spinner_jenis_kendaraan;
+    Spinner spinner_jenis_kendaraan, spinner_nama_shift;
     TextView waktu_masuk;
-    Integer tempIDKendaraan;
+    Integer selectedIDKendaraan, selectedIDShift;
     TextInputEditText nomor_plat;
     Button btnSimpan, btnBatal;
-
-
+    String nomorPlat, waktuMasuk;
+    SessionManager sessionManager;
     //ini untuk load data
     List<Model_Kendaraan> spinnerJenisKendaraanArray = new ArrayList<>();
+    List<Model_Shift> spinnerNamaShiftArray = new ArrayList<>();
+
+    List<String> spinner_IDKendaraan = new ArrayList<>();
     List<String> string_jenisKendaraan = new ArrayList<>();
+
+    List<String> spinner_IDShift = new ArrayList<>();
+    List<String> string_namaShift = new ArrayList<>();
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_tambah_data_kendaraan_masuk);
 
+        sessionManager = new SessionManager(getApplicationContext());
+
         nomor_plat = findViewById(R.id.text_input_platKendaraan);
         spinner_jenis_kendaraan = findViewById(R.id.spinner_jenis_kendaraan);
-        loadJenisKendaraan();
+        spinner_nama_shift = findViewById(R.id.spinner_nama_shift);
 
+        loadNamaShift();
+        loadJenisKendaraan();
+        spinner_nama_shift.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() { //Listener dropdown nama shift saat dipilih
+            @Override
+            public void onItemSelected(AdapterView<?> parentView, View selectedItemView, int position, long id) {
+                selectedIDShift = Integer.parseInt(spinner_IDShift.get(position)); //Mendapatkan id dari dropdown yang dipilih
+                Log.d("Selected ID Shift : ",selectedIDShift.toString());
+            }
+            @Override
+            public void onNothingSelected(AdapterView<?> parentView) {
+                selectedIDShift=1;
+                Log.d("Selected ID Shift : ",selectedIDShift.toString());
+            }
+        });
+        spinner_jenis_kendaraan.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() { //Listener dropdown jenis kendaraan saat dipilih
+            @Override
+            public void onItemSelected(AdapterView<?> parentView, View selectedItemView, int position, long id) {
+                selectedIDKendaraan = Integer.parseInt(spinner_IDKendaraan.get(position)); //Mendapatkan id dari dropdown yang dipilih
+                Log.d("Selected ID Ken : ",selectedIDKendaraan.toString());
+            }
+            @Override
+            public void onNothingSelected(AdapterView<?> parentView) {
+                selectedIDKendaraan=1;
+                Log.d("Selected ID Ken : ",selectedIDKendaraan.toString());
+            }
+        });
         Thread t = new Thread(){
             @Override
             public void run(){
@@ -100,7 +146,7 @@ public class tambah_data_kendaraan_masuk extends AppCompatActivity {
                 .addConverterFactory(GsonConverterFactory.create());
         Retrofit retrofit = builder.build();
 
-        //ngeload nama cabang dari database
+        //ngeload jenis kendaraan dari database
         ApiClient_Kendaraan apiclientKendaraan = retrofit.create(ApiClient_Kendaraan.class);
         Call<LD_Kendaraan> callKendaraan = apiclientKendaraan.show();
         callKendaraan.enqueue(new Callback<LD_Kendaraan>() {
@@ -110,6 +156,7 @@ public class tambah_data_kendaraan_masuk extends AppCompatActivity {
                 spinnerJenisKendaraanArray = response.body().getData();
                 for (int i = 0; i < spinnerJenisKendaraanArray.size(); i++) {
                     string_jenisKendaraan.add(spinnerJenisKendaraanArray.get(i).getJenis_kendaraan());
+                    spinner_IDKendaraan.add(spinnerJenisKendaraanArray.get(i).getId_kendaraan().toString());
                 }
                 ArrayAdapter<String> adapterJenisKendaraan = new ArrayAdapter<>(tambah_data_kendaraan_masuk.this, R.layout.spinner_kendaraan_layout,R.id.txtJenisKendaraan, string_jenisKendaraan);
                 spinner_jenis_kendaraan.setAdapter(adapterJenisKendaraan);
@@ -121,66 +168,91 @@ public class tambah_data_kendaraan_masuk extends AppCompatActivity {
             }
         });
     }
+    void loadNamaShift(){
+        Gson gson = new GsonBuilder()
+                .setLenient()
+                .create();
+        Retrofit.Builder builder = new Retrofit
+                .Builder()
+                .baseUrl(ApiClient_Shift.baseURL)
+                .addConverterFactory(GsonConverterFactory.create());
+        Retrofit retrofit = builder.build();
+
+        //ngeload nama shift dari database
+        ApiClient_Shift apiclientShift = retrofit.create(ApiClient_Shift.class);
+        Call<LD_Shift> callShift = apiclientShift.show();
+        callShift.enqueue(new Callback<LD_Shift>() {
+            @Override
+            public void onResponse(Call<LD_Shift> callShift, Response<LD_Shift> response) {
+
+                spinnerNamaShiftArray = response.body().getData();
+                for (int i = 0; i < spinnerNamaShiftArray.size(); i++) {
+                    string_namaShift.add(spinnerNamaShiftArray.get(i).getNama_shift());
+                    spinner_IDShift.add(spinnerNamaShiftArray.get(i).getId_shift().toString());
+                }
+                ArrayAdapter<String> adapterNamaShift = new ArrayAdapter<>(tambah_data_kendaraan_masuk.this, R.layout.spinner_shift_layout,R.id.txtNamaShift, string_namaShift);
+                spinner_nama_shift.setAdapter(adapterNamaShift);
+            }
+            @Override
+            public void onFailure(Call<LD_Shift> call, Throwable t) {
+                Toast.makeText(tambah_data_kendaraan_masuk.this, t.getLocalizedMessage(), Toast.LENGTH_SHORT).show();
+                Log.d("onFailure: ",t.getLocalizedMessage());
+            }
+        });
+    }
     private void startIntent() {
         Intent intent = new Intent(getApplicationContext(), tampil_data_kendaraan_masuk.class);
         startActivity(intent);
     }
     private void onClickRegister() {
-        if (nomor_plat.getText().toString().isEmpty())
+
+        nomorPlat = nomor_plat.getText().toString();
+        waktuMasuk = waktu_masuk.getText().toString();
+
+        final DateFormat inputFormat =  new SimpleDateFormat("EEE, d MMM yyyy\t\tHH:mm:ss");
+        final DateFormat outputFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+
+        final String inputDateStr=waktuMasuk;
+        Date date = null;
+        try
+        {
+            date = inputFormat.parse(inputDateStr);
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+        final String outputDateStr = outputFormat.format(date);
+
+        if (nomorPlat.isEmpty())
         {
             Toast.makeText(this, "Semua Field harus diisi!", Toast.LENGTH_SHORT).show();
         } else {
-//            Gson gson = new GsonBuilder()
-//                    .setLenient()
-//                    .create();
-//            Retrofit.Builder builder = new Retrofit
-//                    .Builder()
-//                    .baseUrl(ApiClient_Motor.baseURL)
-//                    .addConverterFactory(GsonConverterFactory.create(gson));
-//            Retrofit retrofit = builder.build();
-//            ApiClient_MotorKonsumen apiClientMotorKonsumen= retrofit.create(ApiClient_MotorKonsumen.class);
-//
-//            String[] selectedTipeMotor=spinner_motor.getSelectedItem().toString().split("-");
-//            String[] selectedNamaKonsumen=spinner_konsumen.getSelectedItem().toString().split("-");
-//
-//            for(int i=0; i<spinnerMotorArray.size();i++){
-//
-//                if(spinnerMotorArray.get(i).getTipe_motor().equals(selectedTipeMotor[0]))
-//                {
-//                    tempIDMotor=spinnerMotorArray.get(i).getId_motor();
-//                }
-//            }
-//            Log.d("Selected  Motor : ",selectedTipeMotor[0]);
-//            Log.d("Selected ID Motor : ",tempIDMotor.toString());
-//            for(int i=0; i<spinnerKonsumenArray.size();i++){
-//
-//                if(spinnerKonsumenArray.get(i).getNama_konsumen().equals(selectedNamaKonsumen[0]))
-//                {
-//                    tempIDKonsumen=spinnerKonsumenArray.get(i).getId_konsumen();
-//                }
-//            }
-//            Log.d("Selected  Konsumen : ",selectedNamaKonsumen[0]);
-//            Log.d("Selected ID Konsumen : ",tempIDKonsumen.toString());
-//            sNoPlat = noPlat.getText().toString();
-//
-//            Call<Model_MotorKonsumen> motorKonsumenDAOCall = apiClientMotorKonsumen.create(tempIDMotor,tempIDKonsumen,sNoPlat);
-//            motorKonsumenDAOCall.enqueue(new Callback<Model_MotorKonsumen>() {
-//                @Override
-//                public void onResponse(Call<Model_MotorKonsumen> call, Response<Model_MotorKonsumen> response) {
-//                    if (response.code() == 201) {
-//                        Toast.makeText(tambah_data_motor_konsumen.this, "Tambah Motor Konsumen Sukses!", Toast.LENGTH_SHORT).show();
-//                        startIntent();
-//                    } else {
-//                        Toast.makeText(getApplicationContext(),response.message(), Toast.LENGTH_SHORT).show();
-//                    }
-//                    Log.d("on respon : ",String.valueOf(response.code()));
-//
-//                }
-//                @Override
-//                public void onFailure(Call<Model_MotorKonsumen> call, Throwable t) {
-//                    Toast.makeText(tambah_data_motor_konsumen.this,  t.getLocalizedMessage(), Toast.LENGTH_SHORT).show();
-//                }
-//            });
+            Gson gson = new GsonBuilder()
+                    .setLenient()
+                    .create();
+            Retrofit.Builder builder = new Retrofit
+                    .Builder()
+                    .baseUrl(ApiClient_KendaraanMasuk.baseURL)
+                    .addConverterFactory(GsonConverterFactory.create(gson));
+            Retrofit retrofit = builder.build();
+            ApiClient_KendaraanMasuk apiClientKendaraanMasuk= retrofit.create(ApiClient_KendaraanMasuk.class);
+            Call<Model_KendaraanMasuk> kendaraanMasukDAOCall = apiClientKendaraanMasuk.create(outputDateStr,nomorPlat,selectedIDKendaraan);
+            kendaraanMasukDAOCall.enqueue(new Callback<Model_KendaraanMasuk>() {
+                @Override
+                public void onResponse(Call<Model_KendaraanMasuk> call, Response<Model_KendaraanMasuk> response) {
+                    if (response.code() == 201) {
+                        Toast.makeText(tambah_data_kendaraan_masuk.this, "Tambah Kendaraan Masuk Sukses!", Toast.LENGTH_SHORT).show();
+                        startIntent();
+                    } else {
+                        Toast.makeText(getApplicationContext(),response.message(), Toast.LENGTH_SHORT).show();
+                    }
+                    Log.d("on respon : ",String.valueOf(response.code()));
+
+                }
+                @Override
+                public void onFailure(Call<Model_KendaraanMasuk> call, Throwable t) {
+                    Toast.makeText(tambah_data_kendaraan_masuk .this,  t.getLocalizedMessage(), Toast.LENGTH_SHORT).show();
+                }
+            });
         }
     }
 }
